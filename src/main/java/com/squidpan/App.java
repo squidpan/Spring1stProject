0d8ebf3c79898a2d6e16ea9b790b74e1c1bc4707
squidpan <squidpan11@gmail.com>
2025-12-19 19:19:57 -0500

    getBean byType
    getBean byType

--- src/main/java/com/squidpan/App.java
+++ src/main/java/com/squidpan/App.java
@@ -15,12 +15,13 @@ public class App
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         //each getBeans will get the same single Alien object since default scope is singleton
         //if scope is prototype, obj will be created on demand via getBean and muitiple times
-        Alien obj1 = (Alien) context.getBean("alien1");
+        //Alien obj1 = (Alien) context.getBean("alien1");
+        Alien obj1 = context.getBean("alien1", Alien.class);
 //    	obj1.setAge(21);
         System.out.println(obj1.getAge());
         obj1.code();
 
-        Desktop obj = (Desktop) context.getBean("com2");
+        Desktop obj = context.getBean(Desktop.class);
 
         //obj1.code();
 //
