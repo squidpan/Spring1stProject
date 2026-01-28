13cc75da64b3f7855f1ab19d532f357741222f2d
squidpan <squidpan11@gmail.com>
2026-01-28 11:19:18 -0500

    @Qualifier and @Primary
    @Qualifier and @Primary

--- src/main/java/com/squidpan/App.java
+++ src/main/java/com/squidpan/App.java
@@ -13,12 +13,24 @@ public class App
 {
     public static void main( String[] args )
     {
-        //ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        //Alien obj1=(Alien) context.getBean("alien1");
-        Alien obj1 = context.getBean("alien1", Alien.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Alien obj1 = context.getBean(Alien.class);
+//    	obj1.setAge(21);
         System.out.println(obj1.getAge());
         obj1.code();
+        //Desktop dt = context.getBean("Beast", Desktop.class);
+        //Desktop dt1 = context.getBean("desktop1", Desktop.class);
+        //Desktop dt2 = context.getBean("com2", Desktop.class);
+//        Desktop dt = context.getBean("desktop", Desktop.class);
+//        dt.compile();
+//        Desktop dt1 = context.getBean("desktop", Desktop.class);
+//        dt1.compile();
+//        //ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+//        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+//        //Alien obj1=(Alien) context.getBean("alien1");
+//        Alien obj1 = context.getBean("alien1", Alien.class);
+//        System.out.println(obj1.getAge());
+//        obj1.code();
         //Desktop is lazy-init in spring.xml so dont need this here
         //Desktop obj = (Desktop) context.getBean("com2");
         //Desktop obj = context.getBean("com2",Desktop.class);
--- src/main/java/com/squidpan/config/AppConfig.java
+++ src/main/java/com/squidpan/config/AppConfig.java
@@ -1,18 +1,40 @@
 package com.squidpan.config;
 
+import com.squidpan.Alien;
+import com.squidpan.Computer;
 import com.squidpan.Desktop;
+import com.squidpan.Laptop;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
-
-//    @Bean(name = {"Beast","desktop","com1"})
     @Bean
-    @Scope("prototype")
+    //@Autowired not needed but makes it clear
+    //Autowire Alien and Computer
+    //@Qualifier with name of bean to use
+    //public Alien alien(@Autowired @Qualifier("desktop") Computer com) {
+    public Alien alien(@Autowired Computer com) { //@Qualifier("desktop") instead use @Primary for Laptop just like in xml config
+
+        Alien obj = new Alien();
+        obj.setAge(25);
+        obj.setCom(com);
+        return obj;
+    }
+
+    @Bean
     public Desktop desktop() {
         return new Desktop();
+    }
+
+    @Bean
+    @Primary
+    public Laptop laptop() {
+        return new Laptop();
     }
 }
 
